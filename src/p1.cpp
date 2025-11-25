a5572a3dd4d44fa135182cdd7d6891158067a384
Jay123669 <jay20070623@gmail.com>
2025-11-25 14:19:00 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,10 +1,22 @@
 // 題目 1：高塔建築師 (The Tower Architect) - 20分
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
-    
-    return 0;
+long long factorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n-1);
+}
+
+long long sum(int n){
+    return n*(n+1)/2;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int a=n;
+    cout << factorial(n) << endl;
+    n=a;
+    cout << sum(n) << endl;
 }
